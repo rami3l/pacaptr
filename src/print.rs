da9bc701fd8e4568e8d1acc57ed65a1d304250c7
rami3l <rami3l@outlook.com>
2020-12-25 14:25:08 +0100

    refactor: rewrite `print` using positional argument

--- src/print.rs
+++ src/print.rs
@@ -7,9 +7,11 @@ pub static PROMPT_RUN: &str = "Running";
 pub static PROMPT_INFO: &str = "Info";
 pub static PROMPT_ERROR: &str = "Error";
 
+pub static PROMPT_INDENT: usize = 9;
+
 macro_rules! prompt_format {
     () => {
-        "{:>9}"
+        "{:>indent$}"
     };
 }
 
@@ -33,21 +35,41 @@ macro_rules! question_format {
 
 /// Print out the command after the given prompt.
 pub fn print_cmd<S: AsRef<str>>(cmd: &Cmd<S>, prompt: &str) {
-    println!(cmd_format!(), prompt.green().bold(), cmd)
+    println!(
+        cmd_format!(),
+        prompt.green().bold(),
+        cmd,
+        indent = PROMPT_INDENT
+    )
 }
 
 /// Print out a message after the given prompt.
 pub fn print_msg(msg: &str, prompt: &str) {
-    println!(msg_format!(), prompt.green().bold(), msg);
+    println!(
+        msg_format!(),
+        prompt.green().bold(),
+        msg,
+        indent = PROMPT_INDENT
+    );
 }
 
 /// Print out an error after the given prompt.
 pub fn print_err(err: impl std::fmt::Display, prompt: &str) {
     let err = format!("{:#}", err);
-    eprintln!(msg_format!(), prompt.bright_red().bold(), err);
+    eprintln!(
+        msg_format!(),
+        prompt.bright_red().bold(),
+        err,
+        indent = PROMPT_INDENT
+    );
 }
 
 /// Print out a question after the given prompt.
 pub fn print_question(question: &str, options: &str) {
-    print!(question_format!(), question.yellow(), options.underline());
+    print!(
+        question_format!(),
+        question.yellow(),
+        options.underline(),
+        indent = PROMPT_INDENT
+    );
 }
