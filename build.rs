2e59b6cf90be536a92619c9b6bac3fd13d429e27
rami3l <rami3l@outlook.com>
2021-03-06 18:06:34 +0100

    chore(dist): update build.rs to be ready for crates.io launch

--- build.rs
+++ build.rs
@@ -31,69 +31,73 @@ fn check_methods(file: &Path) -> Result<BTreeMap<String, bool>> {
 }
 
 fn main() -> Result<()> {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed={}", PM_IMPL_DIR);
+    // We only generate compatibility script with debug builds.
+    #[cfg(debug_assertions)]
+    {
+        // Tell Cargo that if the given file changes, to rerun this build script.
+        println!("cargo:rerun-if-changed={}", PM_IMPL_DIR);
 
-    let paths = fs::read_dir(PM_IMPL_DIR).context("Failed while reading PM_IMPL_DIR")?;
+        let paths = fs::read_dir(PM_IMPL_DIR).context("Failed while reading PM_IMPL_DIR")?;
 
-    let excluded_names = ["mod.rs", "unknown.rs"];
+        let excluded_names = ["mod.rs", "unknown.rs"];
 
-    let mut impls = BTreeMap::new();
-    for path in paths {
-        let entry = path.context("Error while reading path")?;
-        if excluded_names.iter().any(|&ex| ex == entry.file_name()) {
-            continue;
+        let mut impls = BTreeMap::new();
+        for path in paths {
+            let entry = path.context("Error while reading path")?;
+            if excluded_names.iter().any(|&ex| ex == entry.file_name()) {
+                continue;
+            }
+            impls.insert(entry.file_name(), check_methods(entry.path().as_ref())?);
         }
-        impls.insert(entry.file_name(), check_methods(entry.path().as_ref())?);
-    }
 
-    let mut table = Table::new();
-    let make_row = |row_name: &str, data: &[&str]| {
-        Row::new({
-            [row_name]
-                .iter()
-                .chain(data.iter())
-                .map(|&s| Cell::new(s))
-                .collect()
-        })
-    };
-
-    // Add first row:
-    // `row!["", "q", "qc", "qe", ..]`
-    table.add_row(make_row("", METHODS));
-
-    for (file, items) in &impls {
-        let data = METHODS
-            .iter()
-            .map(|&method| {
-                let &has_impl = items
-                    .get(method)
-                    .expect("Implementation details not registered");
-                if has_impl {
-                    "*"
-                } else {
-                    ""
-                }
+        let mut table = Table::new();
+        let make_row = |row_name: &str, data: &[&str]| {
+            Row::new({
+                [row_name]
+                    .iter()
+                    .chain(data.iter())
+                    .map(|&s| Cell::new(s))
+                    .collect()
             })
-            .collect::<Vec<_>>();
+        };
 
-        table.add_row(make_row(
-            file.to_str()
-                .context("Failed to convert `file: OsString` to `&str`")?,
-            &data,
-        ));
+        // Add first row:
+        // `row!["", "q", "qc", "qe", ..]`
+        table.add_row(make_row("", METHODS));
+
+        for (file, items) in &impls {
+            let data = METHODS
+                .iter()
+                .map(|&method| {
+                    let &has_impl = items
+                        .get(method)
+                        .expect("Implementation details not registered");
+                    if has_impl {
+                        "*"
+                    } else {
+                        ""
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            table.add_row(make_row(
+                file.to_str()
+                    .context("Failed to convert `file: OsString` to `&str`")?,
+                &data,
+            ));
+        }
+
+        table.set_format(*prettytable::format::consts::FORMAT_CLEAN);
+
+        let mut file = fs::File::create(COMPAT_TABLE_PATH)
+            .context("Failed while creating compatibility table")?;
+
+        file.write_all("```txt\n".as_bytes())?;
+        table
+            .print(&mut file)
+            .context("Failed while writing compatibility table")?;
+        file.write_all("```\n".as_bytes())?;
     }
-
-    table.set_format(*prettytable::format::consts::FORMAT_CLEAN);
-
-    let mut file =
-        fs::File::create(COMPAT_TABLE_PATH).context("Failed while creating compatibility table")?;
-
-    file.write_all("```txt\n".as_bytes())?;
-    table
-        .print(&mut file)
-        .context("Failed while writing compatibility table")?;
-    file.write_all("```\n".as_bytes())?;
 
     Ok(())
 }
