d6e26c91f2c9298c14bff88be1c0764fe2fcea5b
rami3l <rami3l@outlook.com>
2021-03-25 17:21:48 +0100

    refactor: use `respector`

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use pacaptr::{
     dispatch::Opts,
     print::{print_err, PROMPT_ERROR},
 };
+use respector::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    let code = Opts::parse().dispatch().await.unwrap_or_else(|e| {
-        print_err(e, PROMPT_ERROR);
-        1
-    });
+    let code = Opts::parse()
+        .dispatch()
+        .await
+        .inspect_err(|e| print_err(e, PROMPT_ERROR))
+        .unwrap_or(1);
     std::process::exit(code)
 }
