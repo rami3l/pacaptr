15276f0bc7629ba4780e6e5afdd5fe3ad5b0fea5
rami3l <rami3l@outlook.com>
2021-05-04 19:39:59 +0200

    refactor(deps): replace `respector` with `tap`

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use pacaptr::{
     dispatch::Opts,
     print::{print_err, PROMPT_ERROR},
 };
-use respector::prelude::*;
+use tap::TapFallible;
 
 #[tokio::main]
 async fn main() {
     let code = Opts::parse()
         .dispatch()
         .await
-        .inspect_err(|e| print_err(e, PROMPT_ERROR))
+        .tap_err(|e| print_err(e, PROMPT_ERROR))
         .unwrap_or(1);
     std::process::exit(code)
 }
