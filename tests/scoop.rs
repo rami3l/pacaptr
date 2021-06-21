eb5d272e6a214d541871c612135f1114123c5a1c
rami3l <rami3l@outlook.com>
2021-06-21 22:05:33 +0200

    refactor(test): rewrite all `scoop` and `zypper` tests in `test_dsl`

--- tests/scoop.rs
+++ tests/scoop.rs
@@ -5,30 +5,33 @@ use common::*;
 
 #[test]
 fn scoop_si_ok() {
-    Test::new()
-            .pacaptr(&["--using", "scoop", "-Si", "wget"], &[])
-            .output(&["Description: A command-line utility for retrieving files using HTTP, HTTPS, FTP, and FTPS protocols."])
-            .run()
+    test_dsl! { r##"
+        in --using scoop -Si wget
+        ou Description: A command-line utility for retrieving files using HTTP, HTTPS, FTP, and FTPS protocols.
+    "## }
 }
 
 #[test]
 #[should_panic(expected = "Failed with pattern `GNU Wget is not a free software package`")]
 fn scoop_si_fail() {
-    Test::new()
-        .pacaptr(&["--using", "scoop", "-Si", "wget"], &[])
-        .output(&["GNU Wget is not a free software package"])
-        .run()
+    test_dsl! { r##"
+        in --using scoop -Si wget
+        ou GNU Wget is not a free software package
+    "## }
 }
 
 #[test]
 #[ignore]
 fn scoop_r() {
-    Test::new()
-        .pacaptr(&["--using", "scoop", "-S", "wget", "--yes"], &[])
-        .output(&["wget", "was installed successfully!"])
-        .pacaptr(&["--using", "scoop", "-Q"], &[])
-        .output(&["wget", "[main]"])
-        .pacaptr(&["--using", "scoop", "-R", "wget", "--yes"], &[])
-        .output(&["wget", "was uninstalled."])
-        .run()
+    test_dsl! { r##"
+        in --using scoop -S wget --yes
+        ou wget
+        ou was installed successfully!
+        in --using scoop -Q
+        ou wget
+        ou [main]
+        in --using scoop -R wget --yes
+        ou wget
+        ou was uninstalled.
+    "## }
 }
