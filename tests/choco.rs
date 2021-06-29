d8c5568469cc53b03d4afb63b0007fe2f82f9d70
rami3l <rami3l@outlook.com>
2021-06-29 22:33:28 +0200

    feat(test): add more tests for `choco`

--- tests/choco.rs
+++ tests/choco.rs
@@ -4,16 +4,26 @@ mod common;
 use common::*;
 
 #[test]
-fn choco_si_ok() {
+fn choco_q() {
     test_dsl! { r##"
-        in -Si wget
+        in -Q
+        ou Chocolatey
+        in -Q choco
+        ou Chocolatey
+    "## }
+}
+
+#[test]
+fn choco_qi() {
+    test_dsl! { r##"
+        in -Qi wget
         ou GNU Wget is a free software package
     "## }
 }
 
 #[test]
 #[should_panic(expected = "Failed with pattern `GNU Wget is not a free software package`")]
-fn choco_si_fail() {
+fn choco_fail() {
     test_dsl! { r##"
         in -Si wget
         ou GNU Wget is not a free software package
@@ -22,11 +32,27 @@ fn choco_si_fail() {
 
 #[test]
 #[ignore]
-fn choco_r() {
+fn choco_r_s() {
     test_dsl! { r##"
         in -S wget --yes
         ou The install of wget was successful.
         in -R wget --yes
         ou Wget has been successfully uninstalled.
+    "## }
+}
+
+#[test]
+fn choco_si() {
+    test_dsl! { r##"
+        in -Si wget
+        ou GNU Wget is a free software package
+    "## }
+}
+
+#[test]
+fn choco_ss() {
+    test_dsl! { r##"
+        in -Ss wget
+        ou Wget
     "## }
 }
