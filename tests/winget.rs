87d9146a2ca603df6584d3e719914b2ed7e673fc
rami3l <rami3l@outlook.com>
2022-10-16 13:53:06 +0800

    test(pm): add smoke tests for `winget`

---
+++ tests/winget.rs
@@ -0,0 +1,54 @@
+#![cfg(target_os = "windows")]
+
+mod common;
+use common::*;
+
+#[test]
+#[should_panic(expected = "Failed with pattern `GNU Wget is not a free software package`")]
+fn winget_fail() {
+    test_dsl! { r##"
+        in --using winget -Si wget
+        ou GNU Wget is not a free software package
+    "## }
+}
+
+#[test]
+fn winget_qi() {
+    test_dsl! { r##"
+        in --using winget -Qi wget
+        ou Wget is command-line utility for retrieving files using HTTP, HTTPS, FTP, and FTPS protocols.
+    "## }
+}
+
+#[test]
+#[ignore]
+fn winget_r_s() {
+    test_dsl! { r##"
+        in --using winget -S wget --yes
+        ou Found Wget [JernejSimoncic.Wget]
+        ou Successfully installed
+        in --using winget -Q
+        ou Wget
+        ou [JernejSimoncic.Wget]
+        in --using winget -R wget --yes
+        ou Found Wget [JernejSimoncic.Wget]
+        ou Successfully uninstalled
+    "## }
+}
+
+#[test]
+fn winget_si() {
+    test_dsl! { r##"
+        in --using winget -Si wget
+        ou Wget is command-line utility for retrieving files using HTTP, HTTPS, FTP, and FTPS protocols.
+    "## }
+}
+
+#[test]
+fn winget_ss() {
+    test_dsl! { r##"
+        in --using winget -Ss wget
+        ou Wget
+        ou [JernejSimoncic.Wget]
+    "## }
+}
